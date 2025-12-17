e0e4c5bb86ff2d6b5570a9d1db492be334dbb2d2
Alexandr <stitel23@mail.ru>
2025-12-17 16:46:28 +0300

    Add Locations Info

--- src/components/Rick.jsx
+++ src/components/Rick.jsx
@@ -2,22 +2,38 @@ import { useEffect, useState } from "react";
 import { fetchLocations } from "../Api";
 import "./Rick.css";
 
-
 export const Rick = () => {
+    const [locations, setLocations] = useState([]);
+    const [expandedId, setExpandedId] = useState(null);
 
-    const [locations, setLocations] = useState ([]);
-    useEffect (() =>{
+    useEffect(() => {
         fetchLocations().then((data) => {
-            console.log(data);
-        setLocations(data)
+            setLocations(data);
         });
-    },[]);
+    }, []);
 
-    return <div>
-        {locations.map((location) =>{
-            return <div key={location.id} className="location">
-                <h3>{location.name}</h3>
+    const handleToggle = (id) => {
+        setExpandedId(prevId => (prevId === id ? null : id));
+    };
+
+    return (
+        <div>
+            {locations.map((location) => (
+                <div key={location.id} className="location-container">
+                    <div
+                        className="location-title"
+                        onClick={() => handleToggle(location.id)}
+                    >
+                        <h3>{location.name}</h3>
+                    </div>
+                    {expandedId === location.id && (
+                        <div className="location-details">
+                            <p>Тип: {location.type}</p>
+                            <p>Измерение: {location.dimension}</p>
+                        </div>
+                    )}
                 </div>
-        })}
-    </div>
+            ))}
+        </div>
+    );
 };
